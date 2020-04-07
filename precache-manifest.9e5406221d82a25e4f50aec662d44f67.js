2bc7fcb90c109a6f90e0df53e462cbb13f69e2ac
akhil-bp <akhilbp94@gmail.com>
2020-04-07 11:48:03 +0530

    local change

---
+++ precache-manifest.9e5406221d82a25e4f50aec662d44f67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03922ab15dfd8de7b6e2fe6f15a2bdbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d0ff8f94f44b93024fb",
+    "url": "/static/css/main.11c603c3.chunk.css"
+  },
+  {
+    "revision": "45f2d62cb4fa36d4ddc6",
+    "url": "/static/js/2.ec2c6312.chunk.js"
+  },
+  {
+    "revision": "7d0ff8f94f44b93024fb",
+    "url": "/static/js/main.55b04403.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
