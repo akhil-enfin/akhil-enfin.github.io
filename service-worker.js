0d88691da2d3b571c354d5c6e253894469547e6e
akhil-bp <akhilbp94@gmail.com>
2020-04-07 12:18:37 +0530

    local change

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e5406221d82a25e4f50aec662d44f67.js"
+  "/precache-manifest.c144a6cb9e30b416a1a92d8afced1899.js"
 );
 
 self.addEventListener('message', (event) => {
